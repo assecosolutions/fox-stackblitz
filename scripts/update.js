80a2ad330ebf2c9e8a5d4dacc1965316d0b523b7
Florian Ehmke <Florian.Ehmke@assecosol.com>
2022-07-05 15:38:17 +0200

    Remove packages before install / update (in case we nuked a package).

--- scripts/update.js
+++ scripts/update.js
@@ -1,9 +1,10 @@
 const { execSync } = require('child_process');
-const { writeFileSync, appendFileSync } = require('fs');
+const { writeFileSync, appendFileSync, readFileSync } = require('fs');
 
 const updateFoxPackages = () => {
   execSync('rm -f errors/*.log');
   execSync('rm -f src/main.fox.ts');
+  execSync('rm -f package-lock.json');
 
   const packages = JSON.parse(
     execSync(
@@ -14,6 +15,7 @@ const updateFoxPackages = () => {
     .sort();
 
   packages.forEach((pkg) => {
+    console.log('installing / updating: ' + pkg);
     const pkgInfo = JSON.parse(execSync(`npm view ${pkg} --json`));
     const versions = Object.keys(pkgInfo.time);
     const lastPublishedVersion = versions[versions.length - 1];
@@ -34,4 +36,17 @@ const updateFoxPackages = () => {
   });
 };
 
+const removeFoxPackages = () => {
+  const packageJson = JSON.parse(readFileSync('package.json').toString());
+
+  Object.keys(packageJson.dependencies).forEach((p) => {
+    if (p.startsWith('@assecosolutions/fox')) {
+      delete packageJson.dependencies[p];
+    }
+  });
+
+  writeFileSync('package.json', JSON.stringify(packageJson, null, 2));
+};
+
+removeFoxPackages();
 updateFoxPackages();
